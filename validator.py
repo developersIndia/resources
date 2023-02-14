d33ac8901b415a4eead868885c7f9ac1ceb86cd0
Bhupesh Varshney <varshneybhupesh@gmail.com>
2023-02-14 12:39:21 +0000

    add total resource count in validator script

--- validator.py
+++ validator.py
@@ -3,7 +3,7 @@
 """
 Script to validate resources
 """
-import os
+
 from pathlib import Path
 import json
 from jsonschema import validate
@@ -14,30 +14,40 @@ def get_category_list():
     Walk the current directory and get a list of all subdirectories at that
     level. These are the "categories" of resources.
     """
-    dirs = [
-        str(path)
-        for path in Path('.').rglob('*.json')
-    ]
+    dirs = []
+
+    for path in Path('.').rglob('*.json'):
+        path = str(path)
+        if "venv" not in path:
+            dirs.append(path)
+            
     return dirs
 
 
 
 def get_schema():
-    with open("resource.schema", "r") as schema:
-        return schema.read()
+    """
+    Get resource schema content
+    """
+    with open("resource.schema", "r",  encoding='UTF-8') as schema_content:
+        return schema_content.read()
 
 
 def validator(schema, resources):
     """
     Given the resource.schema validate if a give resource index adheres to the rules
     """
+    total_resources = 0
     for resource in resources:
-        with open(resource, "r") as res:
+        with open(resource, "r", encoding='UTF-8') as res:
             data = json.loads(res.read())
+            total_resources = total_resources + len(data["resources"])
             validate(instance=data, schema=schema)
+    print("All resources adhere to the schema ✅")
+    print("Total Resources:", total_resources)
 
 
 if __name__ == "__main__":
     resources = get_category_list()
-    schema = json.loads(get_schema())
-    validator(schema, resources)
+    json_schema = json.loads(get_schema())
+    validator(json_schema, resources)
